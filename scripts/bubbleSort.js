110271d419c46c0ae6f4a066325db9e1995cea1e
cstyan <callumstyan@gmail.com>
2015-02-05 10:32:21 -0800

    still broken

--- scripts/bubbleSort.js
+++ scripts/bubbleSort.js
@@ -1,19 +1,25 @@
 //bubble sort, it's shit
-function bubbleSort (arr) {
-	var len = arr.length();
+function bubbleSort(arr) {
+	var len = arr.length;
 	var swapped = true;
+	console.log("Bubble sort test.");
+	console.log("Before:");
+	console.log(arr.toString());
 
 	while(swapped){
-		swapped = false
+		swapped = false;
 		var i = 0;
 		while(i < (len - 1)){
 			if(arr[i - 1] > arr[i]){
 				//swap
-				var temp = arr[i]
-				arr[i] = arr[i - 1]
-				arr[i - 1] = temp
-				swapped = true
+				var temp = arr[i];
+				arr[i] = arr[i - 1];
+				arr[i - 1] = temp;
+				swapped = true;
 			}
 		}
 	}
+
+	console.log("After:");
+	console.log(arr.toString());
 }
